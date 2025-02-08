da6a69735c6e99983ef30351f986ab64ee8fc33a
Ghost2656 <50245774+Ghost2656@users.noreply.github.com>
2025-02-07 21:34:08 -0500

    Update Merging Table.sql

--- Merging Table.sql
+++ Merging Table.sql
@@ -1,24 +1,24 @@
 --Combine 12 tables into 1
   CREATE TABLE 
-  `centered-sol-442421-n6.Trip_Data.bike_share` AS (
+  `centered-sol-442421-n6.Trip_Data.BikeShare` AS (
   SELECT
     *
   FROM
-    `centered-sol-442421-n6.Trip_Data.TripData_202004`);
+    `centered-sol-442421-n6.Trip_Data.202004_TripData`);
 
--- After I created the table I insert the other tables into it
+-- After I created the table I inserted the other tables into it
 INSERT INTO
-  `centered-sol-442421-n6.Trip_Data.bike_share`
+  `centered-sol-442421-n6.Trip_Data.BikeShare`
 SELECT 
   *
 FROM
-    `centered-sol-442421-n6.Trip_Data.TripData_202005`;
+    `centered-sol-442421-n6.Trip_Data.202005_TripData`;
 -- Did this multiple times 
 
 -- I encountered some errors from some of the tables where some of the columns where a different datatype.
 --So I had to use the CAST() Functions to fix it.
 INSERT INTO
-  `centered-sol-442421-n6.Trip_Data.bike_share`
+  `centered-sol-442421-n6.Trip_Data.BikeShare`
 SELECT
   ride_id,
   rideable_type,
@@ -34,4 +34,4 @@ SELECT
   end_lng,
   member_casual
 FROM
-  `centered-sol-442421-n6.Trip_Data.TripData_202103`
+  `centered-sol-442421-n6.Trip_Data.202103_TripData`
