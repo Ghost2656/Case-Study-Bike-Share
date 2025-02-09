0ed4b573f5d58adf7b4df6c5d823b9436abb8552
Ghost2656 <50245774+Ghost2656@users.noreply.github.com>
2025-02-09 01:42:00 -0500

    Update Merging Table.sql

--- Merging Table.sql
+++ Merging Table.sql
@@ -4,7 +4,7 @@
   SELECT
     *
   FROM
-    `centered-sol-442421-n6.Trip_Data.202004_TripData`);
+    ``centered-sol-442421-n6.Trip_Data.202101_TripData`)
 
 -- After I created the table I inserted the other tables into it
 INSERT INTO
@@ -12,29 +12,10 @@ INSERT INTO
 SELECT 
   *
 FROM
-    `centered-sol-442421-n6.Trip_Data.202005_TripData`;
+    `centered-sol-442421-n6.Trip_Data.202102_TripData`
 -- Did this multiple times 
 
--- I encountered some errors from some of the tables where some columns had a different datatype.
---So I had to use the CAST() Functions to fix it.
-INSERT INTO
-  `centered-sol-442421-n6.Trip_Data.BikeShare`
-SELECT
-  ride_id,
-  rideable_type,
-  started_at,
-  ended_at,
-  start_station_name,
-  SAFE_CAST(start_station_id AS INT64),
-  end_station_name,
-  SAFE_CAST(end_station_id AS INT64),
-  start_lat,
-  start_lng,
-  end_lat,
-  end_lng,
-  member_casual,
-  ride_length,
-  day_of_week,
+
   month
 FROM
   `centered-sol-442421-n6.Trip_Data.202103_TripData`
